fd5313bbca465138260b66711ea0b4fe83d0c457
Tormod Haugland <tormod.haugland@gmail.com>
2022-11-29 16:22:04 +0100

    Add rolling of single dices

--- src/App.js
+++ src/App.js
@@ -1,52 +1,79 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 import DiceContainer from "./components/DiceContainer";
 import DiceButton from "./components/DiceButton";
 
-import DiceThrowMultiple from './assets/audio/KenneyDiceThrowMultiple.ogg';
-import DieThrowSingle from './assets/audio/KenneyDieThrowSingle.ogg';
+import DiceThrowMultiple from "./assets/audio/KenneyDiceThrowMultiple.ogg";
+import DieThrowSingle from "./assets/audio/KenneyDieThrowSingle.ogg";
 
 function App() {
   const [diceValue, setDiceValue] = useState([1, 1]);
   const [isRolling, setIsRolling] = useState([false, false]);
 
-  const playAudio = (audio) => {
+  const playAudio = useCallback((audio) => {
     const soundClip = new Audio(audio);
     soundClip.play();
-  };
+  }, []);
 
-  const animateAllDice = () => {
+  const animateAllDice = useCallback(() => {
     setIsRolling([true, true]);
     setTimeout(() => {
       setIsRolling([false, false]);
     }, 500);
-  };
+  }, []);
 
-  const rollAllDice = () => {
+  const rollAllDice = useCallback(() => {
     animateAllDice();
     playAudio(DiceThrowMultiple);
-    setTimeout(() => {
-      setDiceValue([Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]);
-    }, 200);
-  };
 
-  const handleButtonClick = () => {
-    rollAllDice()
-  };
+    setTimeout(() => {
+      setDiceValue([
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1,
+      ]);
+    }, 200);
+  }, [animateAllDice, playAudio]);
+
+  const handleButtonClick = useCallback(() => {
+    rollAllDice();
+  }, [rollAllDice]);
+
+  const handleSingleDiceClick = useCallback(
+    (index) => {
+      setIsRolling((prevState) => {
+        const newState = [...prevState];
+        newState[index] = true;
+        return newState;
+      });
+      playAudio(DieThrowSingle);
+      setTimeout(() => {
+        setDiceValue((prevState) => {
+          const newState = [...prevState];
+          newState[index] = Math.floor(Math.random() * 6) + 1;
+          return newState;
+        });
+        setIsRolling((prevState) => {
+          const newState = [...prevState];
+          newState[index] = false;
+          return newState;
+        });
+      }, 200);
+    },
+    [playAudio]
+  );
 
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col justify-center space-y-5">
+        <DiceContainer
+          diceValue={diceValue}
+          setDiceValue={setDiceValue}
+          isRolling={isRolling}
+          setIsRolling={setIsRolling}
+          onSingleDiceClick={handleSingleDiceClick}
+        />
 
-      <DiceContainer 
-        diceValue={diceValue} 
-        setDiceValue={setDiceValue} 
-        isRolling={isRolling} 
-        setIsRolling={setIsRolling} 
-      />
-
-      <DiceButton action={handleButtonClick}/>
-      
+        <DiceButton action={handleButtonClick} />
       </div>
     </div>
   );
--- src/components/DiceContainer.js
+++ src/components/DiceContainer.js
@@ -1,21 +1,20 @@
-import Die from './Die';
-import DieThrowSingle from '../assets/audio/KenneyDieThrowSingle.ogg';
+import Die from "./Die";
 
-const DiceContainer = ({ diceValue, setDiceValue, isRolling, setIsRolling }) => {
-
-  const rerollSingleDie = () => {
-    console.log('Functionality not yet implemented');
-  };
-
+const DiceContainer = ({
+  diceValue,
+  setDiceValue,
+  isRolling,
+  setIsRolling,
+  onSingleDiceClick,
+}) => {
   return (
     <div className="flex space-x-5">
-
       <Die
         diceValue={diceValue[0]}
         setDiceValue={setDiceValue}
         isRolling={isRolling[0]}
         setIsRolling={setIsRolling}
-        action={rerollSingleDie}
+        onClick={() => onSingleDiceClick(0)}
       />
 
       <Die
@@ -23,10 +22,8 @@ const DiceContainer = ({ diceValue, setDiceValue, isRolling, setIsRolling }) => 
         setDiceValue={setDiceValue}
         isRolling={isRolling[1]}
         setIsRolling={setIsRolling}
-        action={rerollSingleDie}
+        onClick={() => onSingleDiceClick(1)}
       />
-
-      
     </div>
   );
 };
